15869918c23185da6c8a73681941368409303ba8
deshpande-pavan <pavandeshpande60@gmail.com>
2019-08-04 13:18:45 +0530

    adding bcrypt initial commit

---
+++ src/main/java/com/secureapp/springbootsecurity/AppSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.secureapp.springbootsecurity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	public AuthenticationProvider authProvider(UserDetailsService userDetailsService) {
+		DaoAuthenticationProvider daoProvider = new DaoAuthenticationProvider();
+		daoProvider.setUserDetailsService(userDetailsService);
+		daoProvider.setPasswordEncoder(new BCryptPasswordEncoder());
+		return daoProvider;
+	}
+
+}
